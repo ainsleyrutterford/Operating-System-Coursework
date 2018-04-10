72cc3cb8f007b8688a341f76ed2a36bc54f95350
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2018-04-10 18:08:26 +0100

    Showing that fork structs can be edited by processes.

--- user/dinner.c
+++ user/dinner.c
@@ -10,26 +10,35 @@ void initialise_fork(int id) {
   forks[id].dirty = false;
 }
 
-void philosopher(int i) {
+void philo(int i) {
   char intbuffer[2];
   itoa(intbuffer, i);
   write(STDOUT_FILENO, "Philosopher ", 12);
   write(STDOUT_FILENO, intbuffer, 2);
   write(STDOUT_FILENO, " spawned.\n", 10);
+  forks[i].dirty = true;
   exit(EXIT_SUCCESS);
 }
 
 void main_dinner() {
   for (int i = 0; i < MAX_FORKS; i++) {
     initialise_fork(i);
+    if (!forks[i].dirty) {
+      write( STDOUT_FILENO, "Fork is clean.\n", 15);
+    }
   }
   write( STDOUT_FILENO, "Dinner started...\n", 18 );
   for (int i = 0; i < 16; i++) {
     pid_t pid = fork();
     if (pid == 0) {
-      philosopher(i);
+      philo(i);
     }
   }
   write( STDOUT_FILENO, "Dinner is over.\n", 16 );
+  for (int i = 0; i < MAX_FORKS; i++) {
+    if (forks[i].dirty) {
+      write( STDOUT_FILENO, "Fork is dirty.\n", 15);
+    }
+  }
   exit(EXIT_SUCCESS);
 }
