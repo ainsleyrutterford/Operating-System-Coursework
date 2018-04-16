fc048d1fafe61af9b77e3c857568fa6f553916b6
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2018-04-16 17:45:41 +0100

    philosophers problem finished

--- user/dinner.c
+++ user/dinner.c
@@ -120,10 +120,106 @@ void eat(int id) {
     }
   }
 
-  for (int i = 0; i < 1000000000; i++) {
+}
 
+void listen_for_request_after_eating(int id, int read_fd, int write_fd, char* side) {
+  char buffer[2];
+  char ibuff[2];
+  itoa(ibuff, id);
+
+  read(read_fd, buffer, 2);
+  if (0 == strcmp(buffer, "rq")) {
+    if (0 == strcmp(side, "right")) {
+        write(STDOUT_FILENO, ibuff, 2);
+        write(STDOUT_FILENO, " cleaning right fork and giving it away.\n", 42);
+
+        clean_fork(id, "right");
+        give_fork(id, "right");
+    } else {
+        write(STDOUT_FILENO, ibuff, 2);
+        write(STDOUT_FILENO, " cleaning left fork and giving it away.\n", 41);
+
+        clean_fork(id, "left");
+        give_fork(id, "left");
+    }
+  }
+  write(write_fd, "yy", 2);
+
+}
+
+void listen_for_request_with_one_fork(int id, int read_fd, int write_fd, char* side) {
+  char buffer[2];
+  char ibuff[2];
+  itoa(ibuff, id);
+
+  read(read_fd, buffer, 2);
+  if (0 == strcmp(buffer, "rq")) {
+    if (0 == strcmp(side, "left")) {
+      if (fork_is_dirty(id, "left")) {
+        write(STDOUT_FILENO, ibuff, 2);
+        write(STDOUT_FILENO, " giving dirty left fork away.\n", 30);
+
+        give_fork(id, "left");
+        write(write_fd, "yy", 2);
+      } else {
+        write(STDOUT_FILENO, ibuff, 2);
+        write(STDOUT_FILENO, " keeping clean left fork.\n", 26);
+
+        write(write_fd, "nn", 2);
+      }
+    } else {
+      if (fork_is_dirty(id, "right")) {
+        write(STDOUT_FILENO, ibuff, 2);
+        write(STDOUT_FILENO, " giving dirty right fork away.\n", 31);
+
+        give_fork(id, "right");
+        write(write_fd, "yy", 2);
+      } else {
+        write(STDOUT_FILENO, ibuff, 2);
+        write(STDOUT_FILENO, " keeping clean right fork.\n", 27);
+
+        write(write_fd, "nn", 2);
+      }
+    }
   }
 
+}
+
+void request_fork_and_eat_if_possible(int id, int left_read_fd, int left_write_fd, int right_read_fd, int right_write_fd, char* side) {
+  char buffer[2];
+  char ibuff[2];
+  itoa(ibuff, id);
+
+  if (0 == strcmp(side, "left")) {
+    write(STDOUT_FILENO, ibuff, 2);
+    write(STDOUT_FILENO, " requesting left fork.\n", 23);
+
+    write(left_write_fd, "rq", 2);
+    read(left_read_fd, buffer, 2);
+    if (0 == strcmp(buffer, "yy")) {
+      // The fork has been cleaned and given to us
+      write(STDOUT_FILENO, ibuff, 2);
+      write(STDOUT_FILENO, " obtained left fork.\n", 21);
+    }
+  } else {
+    write(STDOUT_FILENO, ibuff, 2);
+    write(STDOUT_FILENO, " requesting right fork.\n", 24);
+
+    write(right_write_fd, "rq", 2);
+    read(right_read_fd, buffer, 2);
+    if (0 == strcmp(buffer, "yy")) {
+      // The fork has been cleaned and given to us
+      write(STDOUT_FILENO, ibuff, 2);
+      write(STDOUT_FILENO, " obtained right fork.\n", 22);
+    }
+  }
+  if (0 == strcmp(buffer, "yy")) {
+    if (can_eat(id)) {
+      eat(id);
+      listen_for_request_after_eating(id, right_read_fd, right_write_fd, "right");
+      listen_for_request_after_eating(id, left_read_fd, left_write_fd, "left");
+    }
+  }
 }
 
 void philo(int id, int left_read_fd, int left_write_fd, int right_read_fd, int right_write_fd) {
@@ -136,93 +232,21 @@ void philo(int id, int left_read_fd, int left_write_fd, int right_read_fd, int r
     itoa(ibuff, id);
 
     if (can_eat(id)) {
-
       eat(id);
-
-      read(right_read_fd, buffer, 2);
-      if (0 == strcmp(buffer, "rq")) {
-
-        write(STDOUT_FILENO, ibuff, 2);
-        write(STDOUT_FILENO, " cleaning right fork and giving it away.\n", 42);
-
-        clean_fork(id, "right");
-        give_fork(id, "right");
-        write(right_write_fd, "yy", 2);
-      }
-
-      read(left_read_fd, buffer, 2);
-      if (0 == strcmp(buffer, "rq")) {
-
-        write(STDOUT_FILENO, ibuff, 2);
-        write(STDOUT_FILENO, " cleaning left fork and giving it away.\n", 41);
-
-        clean_fork(id, "left");
-        give_fork(id, "left");
-        write(left_write_fd, "yy", 2);
-      }
-
+      listen_for_request_after_eating(id, right_read_fd, right_write_fd, "right");
+      listen_for_request_after_eating(id, left_read_fd, left_write_fd, "left");
     } else {
 
       if (has_fork(id, "left")) {
-
-        read(left_read_fd, buffer, 2);
-        if (0 == strcmp(buffer, "rq")) {
-          if (fork_is_dirty(id, "left")) {
-            give_fork(id, "left");
-            write(left_write_fd, "yy", 2);
-          } else {
-            write(left_write_fd, "nn", 2);
-          }
-        }
-
+        listen_for_request_with_one_fork(id, left_read_fd, left_write_fd, "left");
       } else {
-
-        write(left_write_fd, "rq", 2);
-        read(left_read_fd, buffer, 2);
-        if (0 == strcmp(buffer, "yy")) {
-          // The fork has been cleaned and given to us
-          if (can_eat(id)) {
-            eat(id);
-            read(right_read_fd, buffer, 2);
-            if (0 == strcmp(buffer, "rq")) {
-              clean_fork(id, "right");
-              give_fork(id, "right");
-              write(right_write_fd, "yy", 2);
-            }
-
-            read(left_read_fd, buffer, 2);
-            if (0 == strcmp(buffer, "rq")) {
-              clean_fork(id, "left");
-              give_fork(id, "left");
-              write(left_write_fd, "yy", 2);
-            }
-          }
-        }
-
+        request_fork_and_eat_if_possible(id, left_read_fd, left_write_fd, right_read_fd, right_write_fd, "left");
       }
 
       if (has_fork(id, "right")) {
-
-        read(right_read_fd, buffer, 2);
-        if (0 == strcmp(buffer, "rq")) {
-          if (fork_is_dirty(id, "right")) {
-            give_fork(id, "right");
-            write(left_write_fd, "yy", 2);
-          } else {
-            write(left_write_fd, "nn", 2);
-          }
-        }
-
+        listen_for_request_with_one_fork(id, right_read_fd, right_write_fd, "right");
       } else {
-
-        write(right_write_fd, "rq", 2);
-        read(right_read_fd, buffer, 2);
-        if (0 == strcmp(buffer, "yy")) {
-          // The fork has been cleaned and given to us
-          // write(STDOUT_FILENO, ibuff, 2);
-          // write(STDOUT_FILENO, " received right fork.\n", 22);
-        }
-
+        request_fork_and_eat_if_possible(id, left_read_fd, left_write_fd, right_read_fd, right_write_fd, "right");
       }
 
     }
